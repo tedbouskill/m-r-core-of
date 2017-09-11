6815ac18159c52d4788fb97d64f2699957ea0e70
Ted Bouskill <tedbouskill@Teds-MacBook-Pro.local>
2017-09-10 20:46:48 -0700

    Additional files used for full event sourcing

---
+++ EventSourcingCQRS/Common/EventSourcing/AModelEventData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Common.EventSourcing.Interfaces;
+
+namespace Common.EventSourcing
+{
+    /// <summary>
+    /// Data used to change a models state in an event
+    /// </summary>
+    public abstract class AModelEventData<KeyT> : IModelEventData<KeyT>
+    {
+        /// <summary>
+        /// This object should know how to change the state of the model
+        /// </summary>
+        /// <param name="model">The model that needs to be changed</param>
+        public abstract void ApplyEventData(IModelAggregate<KeyT> model);
+    }
+}
---
+++ EventSourcingCQRS/Common/EventSourcing/AModelEvents.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Common.EventSourcing.Interfaces;
+
+namespace Common.EventSourcing
+{
+    /// <summary>
+    /// Represents an aggregate of Event Source items for an Aggregate Model
+    /// </summary>
+    public abstract class AModelEvents<KeyT> : IModelEvents<KeyT>
+    {
+        /// <summary>
+        /// An event store capable of storing a set of events using one key in the order events are added
+        /// </summary>
+        private IEventStore<KeyT> _eventStore;
+
+        private KeyT _aggregateKey;
+
+        public AModelEvents(IEventStore<KeyT> eventStore, KeyT aggregateKey)
+        {
+            _aggregateKey = aggregateKey;
+            _eventStore = eventStore;
+        }
+
+        public async Task<IEnumerable<IModelEvent<KeyT>>> EventsAsync()
+        {
+            return await _eventStore.Events(_aggregateKey);
+        }
+
+        /// <summary>
+        /// Appends the event model
+        /// </summary>
+        /// <returns>The aggregate model</returns>
+        /// <param name="eventModel">Event source model.</param>
+        public async Task<int> AppendEventAsync(IModelEvent<KeyT> eventModel)
+        {
+            return await _eventStore.AppendEvent(eventModel);
+        }
+
+        /// <summary>
+        /// The aggregate model for all the events
+        /// </summary>
+        /// <returns>The aggregate model</returns>
+        public abstract Task<ModelAggregate<KeyT>> ModelAsync();
+    }
+}
