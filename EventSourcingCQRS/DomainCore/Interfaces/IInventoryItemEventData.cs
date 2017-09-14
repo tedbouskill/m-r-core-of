d69487839a0a2606e9f0329676be7c9c4570da3c
Ted Bouskill <tedbouskill@Teds-MBP.hitronhub.home>
2017-09-13 19:17:43 -0700

    Added new Interface for inventory item event data

---
+++ EventSourcingCQRS/DomainCore/Interfaces/IInventoryItemEventData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DomainCore.Interfaces
+{
+    /// <summary>
+    /// Mutable inventory model data that can be changed by user events
+    /// </summary>
+    public interface IInventoryItemEventData
+    {
+    }
+}
