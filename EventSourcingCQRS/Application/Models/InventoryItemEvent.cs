0b704e3b0994e1334036e15f495bfbb184ead722
Ted Bouskill <tedbouskill@Teds-MBP.hitronhub.home>
2017-09-13 19:15:50 -0700

    Moved from DomainCore

---
+++ EventSourcingCQRS/Application/Models/InventoryItemEvent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Common.EventSourcing.Interfaces;
+using DomainCore.Interfaces;
+using Newtonsoft.Json;
+
+namespace Application.Models
+{	
+    /// <summary>
+	/// Represents the changes to an inventory item based on an event
+	/// </summary>
+	public class InventoryItemEvent : IInventoryItemEvent
+    {
+        public Guid AggregateId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string EventName { get; set; }
+
+        public object EventData { get; set; }
+
+        public InventoryItemEvent(){}
+
+        public InventoryItemEvent(IInventoryItemEvent inventoryItemEvent)
+        {
+            AggregateId = inventoryItemEvent.AggregateId;
+            Timestamp = inventoryItemEvent.Timestamp;
+            EventName = inventoryItemEvent.EventName;
+            EventData = inventoryItemEvent.EventData;
+        }
+
+        public void ApplyEventData(IModelAggregate<Guid> model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string DataAsJson
+        {
+            get
+			{
+				return JsonConvert.SerializeObject(this.EventData);
+			}
+		}
+    }
+}
