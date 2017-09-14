b4a15f4e4540e6c9a33a31c5f1e7ba0cc63e62ec
Ted Bouskill <tedbouskill@Teds-MBP.hitronhub.home>
2017-09-13 19:24:08 -0700

    Moved base inventory item data to a model

--- EventSourcingCQRS/DomainCore/Interfaces/IInventoryItemData.cs
+++ EventSourcingCQRS/DomainCore/Interfaces/IInventoryItemData.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-
 namespace DomainCore.Interfaces
 {
-    /// <summary>
-    /// Mutable inventory model data that can be changed by user events
-    /// </summary>
     public interface IInventoryItemData
     {
 		string Name { get; set; }
-
 		bool IsActive { get; set; }
-
 		int Count { get; set; }
-
 		string Note { get; set; }
-	}
+    }
 }
