e379248de30d28e965139c3176d86ee5a178e0ff
Ted Bouskill <tedbouskill@Teds-MacBook-Pro.local>
2017-09-14 19:34:07 -0700

    Added support for InventoryItemAggregate

--- EventSourcingCQRS/DomainCore/InventoryItemDto.cs
+++ EventSourcingCQRS/DomainCore/InventoryItemDto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using DomainCore.Interfaces;
+
 namespace DomainCore
 {
     /// <summary>
@@ -7,13 +9,25 @@ namespace DomainCore
     /// </summary>
     public class InventoryItemDto : Interfaces.IInventoryItem
     {
+        public InventoryItemDto() { }
+        
+        public InventoryItemDto(InventoryItemAggregate inventoryItemAggregate)
+        {
+            Id = inventoryItemAggregate.AggregateId;
+            LastEventTimestamp = inventoryItemAggregate.LastEventTimestamp;
+            Name = inventoryItemAggregate.Name;
+			IsActive = inventoryItemAggregate.IsActive;
+			Count = inventoryItemAggregate.Count;
+			Note = inventoryItemAggregate.Note;
+		}
+
 		public Guid Id { get; set; }
 
 		public DateTime LastEventTimestamp { get; set; }
 
         public string Name { get; set; }
-        public bool IsActive { get; set; } = false;
-        public int Count { get; set; } = 0;
+        public bool IsActive { get; set; }
+        public int Count { get; set; }
         public string Note { get; set; }
     }
 }
