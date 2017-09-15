7a4367bb764ce4ec94eb7f380cc95e50727053c5
Ted Bouskill <tedbouskill@Teds-MacBook-Pro.local>
2017-09-14 19:24:12 -0700

    Added EditInventoryItem model to WebApp to track changes to the original InventoryItem

---
+++ EventSourcingCQRS/WebApp/Models/EditInventoryItem.cs
@@ -0,0 +1,26 @@
+﻿using Application.Models;
+using DomainCore;
+
+namespace WebApp.Models
+{
+    public class EditInventoryItem
+    {
+        public EditInventoryItem() {}
+
+        public EditInventoryItem(InventoryItemDto inventoryItem)
+        {
+            Original = new InventoryItemData()
+            {
+                Name = inventoryItem.Name,
+                IsActive = inventoryItem.IsActive,
+                Count = inventoryItem.Count,
+                Note = (inventoryItem.Note ?? "")
+            };
+            Editable = inventoryItem;
+        }
+
+        public InventoryItemData Original { get; set; }
+
+        public InventoryItemDto Editable { get; set; }
+    }
+}
