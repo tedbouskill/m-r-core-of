0256f42a862c4b14b895860b7bc2187902ad0671
Ted Bouskill <tedbouskill@Teds-MBP.hitronhub.home>
2018-07-07 23:14:38 -0700

    Fixed configuration settings

--- EventSourcingCQRS/Application/IServiceCollectionExtension.cs
+++ EventSourcingCQRS/Application/IServiceCollectionExtension.cs
@@ -1,8 +1,5 @@
 ﻿using Application.Interfaces;
-using Infrastructure.Data;
-using Infrastructure.Data.Interfaces;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
+using Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,37 +10,7 @@ namespace Application
         public static IServiceCollection AddApplicationServices(
             this IServiceCollection services, IConfiguration configuration)
 		{
-            {
-                var options = new DbContextOptionsBuilder<InventoryDbContext>()
-                    .UseSqlite(new SqliteConnection(configuration.GetConnectionString("InventoryDbContext")))
-                    .Options;
-
-                // Create the schema in the database
-                using (var dbContext = new InventoryDbContext(options))
-                {
-                    dbContext.Database.EnsureCreated();
-                }
-            }
-
-			// DbContexts
-			services.AddDbContext<InventoryItemsDbContext>(options =>
-					options.UseSqlite(configuration.GetConnectionString("InventoryDbContext")));
-			
-            services.AddDbContext<InventoryItemsReadDbContext>(options =>
-					options.UseSqlite(configuration.GetConnectionString("InventoryDbContext")));
-			services.AddDbContext<InventoryItemsWriteDbContext>(options =>
-		            options.UseSqlite(configuration.GetConnectionString("InventoryDbContext")));
-			
-            services.AddDbContext<InventoryEventsDbContext>(options =>
-					options.UseSqlite(configuration.GetConnectionString("InventoryDbContext")));
-
-            // Repositories
-			services.AddScoped<IInventoryEventRepository, InventoryEventRepository>();
-
-			services.AddScoped<IInventoryReadRepository, InventoryReadRepository>();
-			services.AddScoped<IInventoryWriteRepository, InventoryWriteRepository>();
-
-			services.AddScoped<IInventoryRepository, InventoryRepository>();
+            services.AddInfrastructureServices(configuration);
 
             // Services
             services.AddScoped<IInventoryCommandHandler, InventoryCommandHandler>();
--- EventSourcingCQRS/Infrastructure/IServiceCollectionExtension.cs
+++ EventSourcingCQRS/Infrastructure/IServiceCollectionExtension.cs
@@ -1,5 +1,6 @@
 ﻿using Infrastructure.Data;
 using Infrastructure.Data.Interfaces;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,25 +11,37 @@ namespace Infrastructure
 	{
 		public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
 		{
-			// DbContexts
-			services.AddDbContext<InventoryItemsDbContext>(options =>
-					options.UseSqlite(configuration.GetConnectionString("InventoryDbContext")));
+            {
+                var options = new DbContextOptionsBuilder<InventoryDbContext>()
+                    .UseSqlite(new SqliteConnection(configuration.GetConnectionString("InventoryDbContext")))
+                    .Options;
 
-			services.AddDbContext<InventoryItemsReadDbContext>(options =>
-					options.UseSqlite(configuration.GetConnectionString("InventoryDbContext")));
-			services.AddDbContext<InventoryItemsWriteDbContext>(options =>
-					options.UseSqlite(configuration.GetConnectionString("InventoryDbContext")));
+                // Create the schema in the database
+                using (var dbContext = new InventoryDbContext(options))
+                {
+                    dbContext.Database.EnsureCreated();
+                }
+            }
 
-			services.AddDbContext<InventoryEventsDbContext>(options =>
-					options.UseSqlite(configuration.GetConnectionString("InventoryDbContext")));
+            // DbContexts
+            services.AddDbContext<InventoryItemsDbContext>(options =>
+                    options.UseSqlite(configuration.GetConnectionString("InventoryDbContext")));
+            
+            services.AddDbContext<InventoryItemsReadDbContext>(options =>
+                    options.UseSqlite(configuration.GetConnectionString("InventoryDbContext")));
+            services.AddDbContext<InventoryItemsWriteDbContext>(options =>
+                    options.UseSqlite(configuration.GetConnectionString("InventoryDbContext")));
+            
+            services.AddDbContext<InventoryEventsDbContext>(options =>
+                    options.UseSqlite(configuration.GetConnectionString("InventoryDbContext")));
 
             // Repositories
-			services.AddScoped<IInventoryEventRepository, InventoryEventRepository>();
+            services.AddScoped<IInventoryEventRepository, InventoryEventRepository>();
 
-			services.AddScoped<IInventoryReadRepository, InventoryReadRepository>();
-			services.AddScoped<IInventoryWriteRepository, InventoryWriteRepository>();
+            services.AddScoped<IInventoryReadRepository, InventoryReadRepository>();
+            services.AddScoped<IInventoryWriteRepository, InventoryWriteRepository>();
 
-			services.AddScoped<IInventoryRepository, InventoryRepository>();
+            services.AddScoped<IInventoryRepository, InventoryRepository>();
 
 			return services;
 		}
